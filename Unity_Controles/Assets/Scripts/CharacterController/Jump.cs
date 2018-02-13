6fa07f002bb60beb23562dc719117aa879bed1da
HP-D\Divya <divya.higelin@gmail.com>
2018-02-13 21:22:34 +0100

    Controles de déplacement et de saut

---
+++ Unity_Controles/Assets/Scripts/CharacterController/Jump.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Jump : MonoBehaviour {
+	Coroutine jump;
+	Rigidbody2D rigid;
+	public Transform groundCheck;
+	bool isJumping;
+	float startPos;
+	public LayerMask ground;
+	public float jumpForce = 2f;
+
+	void Awake () 
+    {
+		isJumping = false;
+		rigid = GetComponent<Rigidbody2D>();
+	}
+
+	bool IsGrounded()
+	{
+		return (Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground));
+	}
+
+	public void OnTouchDown() {
+		if (isJumping)
+			return;
+		isJumping = true;
+		var touchCount = Input.touchCount - 1;
+		startPos = Input.GetTouch(touchCount).position.y;
+		jump = StartCoroutine(IsJumping());
+	}
+
+	IEnumerator IsJumping() {
+		var currentTouch = Input.touchCount - 1;
+		var touchId = Input.GetTouch(currentTouch).fingerId;
+		while (true)
+		{
+			for (int i = 0; i < Input.touchCount; i++)
+			{
+				if (Input.GetTouch(i).fingerId == touchId)
+				{
+					currentTouch = i;
+					break;
+				}
+			}
+			var distance = Input.GetTouch(currentTouch).position.y - startPos;
+			if ((distance > 10 && IsGrounded()) || distance < -10)
+			{
+				distance = Mathf.Clamp(distance, -10, 10);
+				rigid.AddForce(new Vector2(0, distance * jumpForce), ForceMode2D.Impulse);
+				startPos = Input.GetTouch(currentTouch).position.y;
+			}
+			yield return null;
+		}
+	}
+
+	public void OnTouchUp() {
+		isJumping = false;
+		StopCoroutine(jump);
+	}
+}
---
+++ Unity_Controles/Assets/Scripts/CharacterController/Move.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Move : MonoBehaviour {
+	public Slider slider;
+	Coroutine move;
+	Rigidbody2D rigid;
+	bool isMoving;
+	public float sliderDistance = 10;
+	float startPos;
+	public Vector2 minSliderPos;
+	public Vector2 maxSliderPos;
+	public float speed = 2;
+
+	void Awake() {
+		isMoving = false;
+		slider.gameObject.SetActive(false);
+		rigid = GetComponent<Rigidbody2D>();
+		minSliderPos.x = Screen.width / 10;
+		maxSliderPos.x = Screen.width / 2;
+		maxSliderPos.y = Screen.height - (Screen.height / 10);
+	}
+
+	public void OnTouchDown() {
+		if (isMoving)
+			return;
+		isMoving = true;
+		slider.gameObject.SetActive(true);
+		startPos = Input.GetTouch(Input.touchCount - 1).position.x;
+		move = StartCoroutine(IsMoving());
+	}
+
+	IEnumerator IsMoving() {
+		var currentTouch = Input.touchCount - 1;
+		var touchId = Input.GetTouch(currentTouch).fingerId;
+		while (true)
+		{
+			for (int i = 0; i < Input.touchCount; i++)
+			{
+				if (Input.GetTouch(i).fingerId == touchId)
+				{
+					currentTouch = i;
+					break;
+				}
+			}
+			var tmp = new Vector2(Input.GetTouch(currentTouch).position.x, Input.GetTouch(currentTouch).position.y + sliderDistance);
+			tmp.x = Mathf.Clamp(tmp.x, minSliderPos.x, maxSliderPos.x);
+			tmp.y = Mathf.Clamp(tmp.y, minSliderPos.y, maxSliderPos.y);
+			slider.gameObject.transform.position = tmp;
+			slider.value = Input.GetTouch(currentTouch).position.x - startPos;
+			rigid.velocity = new Vector2(slider.value * speed * Time.deltaTime, rigid.velocity.y);
+			yield return null;
+		}
+	}
+
+	public void OnTouchUp() {
+		isMoving = false;
+		slider.gameObject.SetActive(false);
+		StopCoroutine(move);
+	}
+}
