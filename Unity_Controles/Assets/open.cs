4db77e9408fa370bd902427e6a940483150b8f60
Stephanie <stephanie.aubenneau@mainbot.me>
2018-03-04 16:20:29 +0100

    Plateformes + bourgeon

---
+++ Unity_Controles/Assets/open.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class open : MonoBehaviour
+{
+
+	private void OnCollisionEnter2D(Collision2D col) 
+	{
+		print(col.gameObject.name);
+		if(col.gameObject.name == "Player")
+		{
+			foreach (Transform child in transform)
+			{
+				child.gameObject.SetActive(true);
+			}
+		}
+	}
+}
