4269885e9d38584eff1e7ef2b73f8745de378d6c
HP-D\Divya <divya.higelin@gmail.com>
2018-03-05 17:16:46 +0100

    Bourgeons

---
+++ Unity_Controles/Assets/Scripts/GPE/Bourgeon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Bourgeon : MonoBehaviour {
+	public LayerMask PlayerLayer;
+	public GameObject Plateformes;
+	public float CastRadius = 1f;
+
+	bool IsPlayerAround () 
+	{
+		if (Physics2D.OverlapCircle(transform.position, CastRadius, PlayerLayer))
+			return (true);
+		return (false);
+	}
+
+	private void OnMouseUpAsButton()
+	{
+		if (IsPlayerAround() && !Plateformes.activeSelf)
+			EnablePlatforms();
+	}
+
+	void EnablePlatforms()
+	{
+		Plateformes.SetActive(true);
+	}
+}
